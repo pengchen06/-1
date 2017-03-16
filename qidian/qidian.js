6323fa0b6bb7086599dc119bb85a5e67c5502fea
chen peng <peng.chen06@hand-china.com>
2017-03-16 18:08:03 +0800

    起点

---
+++ qidian/qidian.js
@@ -0,0 +1,83 @@
+/**
+ * Created by pengc on 2017/3/15.
+ */
+function show() {
+    var a=document.getElementById("read");
+    var b=document.getElementById("mread");
+    b.style.backgroundColor="aqua";
+    b.style.borderTop="2px solid red"
+    a.style.display="block";
+}
+function hid() {
+    var a=document.getElementById("read");
+    var b=document.getElementById("mread");
+    b.style.backgroundColor="#f7f5f0";
+    b.style.borderTopColor="#f7f5f0";
+    a.style.display="none";
+}
+function show1() {
+    var a=document.getElementById("read1");
+    var b=document.getElementById("mread1");
+    b.style.backgroundColor="blue";
+    b.style.borderTop="2px solid red"
+    a.style.display="block";
+}
+function hid1() {
+    var a=document.getElementById("read1");
+    var b=document.getElementById("mread1");
+    b.style.backgroundColor="#f7f5f0";
+    b.style.borderTopColor="#f7f5f0";
+    a.style.display="none";
+}
+function show2() {
+    var a=document.getElementById("read2");
+    var b=document.getElementById("mread2");
+    b.style.backgroundColor="green";
+    b.style.borderTop="2px solid red"
+    a.style.display="block";
+}
+function hid2() {
+    var a=document.getElementById("read2");
+    var b=document.getElementById("mread2");
+    b.style.backgroundColor="#f7f5f0";
+    b.style.borderTopColor="#f7f5f0";
+    a.style.display="none";
+}
+function tshow() {
+    var a=document.getElementById("title0");
+    var b=document.getElementById("title");
+    b.style.backgroundColor="#ffffff";
+    b.style.borderTop="2px solid red"
+    a.style.display="block";
+}
+function thid() {
+    var a=document.getElementById("title0");
+    var b=document.getElementById("title");
+    b.style.backgroundColor="#f7f5f0";
+    b.style.borderTopColor="#f7f5f0";
+    a.style.display="none";
+}
+function t1(){
+    var a=document.getElementById("t");
+    a.style.backgroundColor="gray"
+}
+function t11(){
+    var a=document.getElementById("t");
+    a.style.backgroundColor="#ffffff";
+}
+function t2(){
+    var a=document.getElementById("tt");
+    a.style.backgroundColor="gray"
+}
+function t22(){
+    var a=document.getElementById("tt");
+    a.style.backgroundColor="#ffffff";
+}
+function t3(){
+    var a=document.getElementById("ttt");
+    a.style.backgroundColor="gray"
+}
+function t33(){
+    var a=document.getElementById("ttt");
+    a.style.backgroundColor="#ffffff";
+}
